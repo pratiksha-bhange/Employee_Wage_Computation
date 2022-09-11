ef4e2fe3d00fa36255806bea425ae35dee2395a7
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-11 19:07:27 +0530

    [Pratu]ADDED: Solving Using Case Statement

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,31 +4,29 @@ namespace EmployeeWageComputation
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome To Employee Wage Computation Program On Master Branch");
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
 
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            //Variables
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            //Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee wages is=" + empWage);
