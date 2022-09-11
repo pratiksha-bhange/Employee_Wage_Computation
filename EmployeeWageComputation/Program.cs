aa08c4cafdb2377ffa55bc798eb21406b92a4636
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-11 16:54:36 +0530

    [Pratu]Added: Ability To Check Employee Wage computation

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Computation Program On Master Branch");
+            
+        }
+    }
+}
