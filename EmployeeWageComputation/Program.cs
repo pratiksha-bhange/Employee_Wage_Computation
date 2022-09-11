8f77ae73d5f858153023fecb41457e823dad4cfa
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-11 17:00:36 +0530

    [Pratu]ADDED: Check Empolyee Is Present Or Absent

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,7 +7,21 @@ namespace EmployeeWageComputation
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program On Master Branch");
-            
+
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is prsent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
         }
     }
 }
