18b88f07a40291dff6dde51bf20bf5455200ea24
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-11 18:59:02 +0530

    [Pratu]ADDED: Calculate Part Time Employee Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,13 +8,21 @@ namespace EmployeeWageComputation
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program On Master Branch");
 
-            int IS_FULL_TIME = 1;
+            //Constants
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
+            //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            //Computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
