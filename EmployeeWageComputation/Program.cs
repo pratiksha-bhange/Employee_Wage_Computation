1f6ed7c95738a5b798147c6f7339ca4ba47f530f
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-11 18:52:03 +0530

    [Pratu]ADDED: Calculate Daily Employee Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,20 +8,23 @@ namespace EmployeeWageComputation
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program On Master Branch");
 
-            //Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is prsent");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
-
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wages is=" + empWage);
         }
     }
+    
 }
