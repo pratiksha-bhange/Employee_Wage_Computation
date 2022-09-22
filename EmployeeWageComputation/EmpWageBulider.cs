4e51ca366b1f54e0774509f4cd6a7f53dd9f56f7
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-22 23:11:29 +0530

    [Pratu]ADDED:Compute Employee Wage for multiple companies

---
+++ EmployeeWageComputation/EmpWageBulider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage
+{
+    class EmpWageBuilder
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWages;
+
+        public EmpWageBuilder(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void computeEmpWage()
+        {
+            int empHrs = 0;
+            int totalWorkingDays = 0;
+            int totalEmployeeHour = 0;
+            while (empHrs <= maxHoursPerMonth && totalWorkingDays <= numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmployeeHour += empHrs;
+                Console.WriteLine("Day:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            int totalEmpWages = totalEmployeeHour * this.empRatePerHour;
+            Console.WriteLine("Total employee wage for company:" + company + " is " + totalEmpWages);
+        }
+        public string toString()
+        {
+            return "Total employee wage for company:" + this.company + " is " + this.totalEmpWages;
+        }
+    }
+}
---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Employee_Wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder dMart = new EmpWageBuilder("DMart", 20, 2, 10);
+            EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+        }
+    }
+}
+    
+
