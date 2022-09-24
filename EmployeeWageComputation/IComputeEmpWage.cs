549ad0989d7743559b686a55398d6dc3f520a45b
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-24 20:11:03 +0530

    [Pratu]ADDED: Ability to mange Emp

---
+++ EmployeeWageComputation/IComputeEmpWage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    interface IComputeEmpWage
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        public void computeEmpWage();
+    }
+}
---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee_Wage
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
